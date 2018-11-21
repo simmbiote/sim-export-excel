a925556642e76526ffdabd34d2a69e81f77c351a
John Simms <john@gsdh.org>
2018-11-21 14:49:31 +0200

    version

--- sim-export-excel.php
+++ sim-export-excel.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Sim Excel Export
  * Plugin URI:        sim-export-excel
  * Description:       Extend existing features of your backend to enable proper Excel exports (not just CSV) of data.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            John Simms
  * Author URI:        https://www.jbsimms.co.za
  * License:           GPL-2.0+
@@ -37,7 +37,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.1' );
+define( 'PLUGIN_NAME_VERSION', '1.0.2' );
 
 /**
  * The code that runs during plugin activation.
